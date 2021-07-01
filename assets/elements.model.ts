f262121f662e0d392d2e59635a5655145e7fd2dd
rajbattiwala <rajbattiwala@gmail.com>
2021-07-01 13:18:10 +0530

    Auto-generated commit [ci skip]

---
+++ assets/elements.model.ts
@@ -0,0 +1,11 @@
+export class elements{
+    id: number | undefined;
+    state: boolean | undefined;
+    name: string | undefined;
+    code: string | undefined;
+    type: string | undefined;
+    yield: string | undefined;
+    category: string | undefined;
+    cost: string | undefined;
+    stock: string | undefined;
+}
