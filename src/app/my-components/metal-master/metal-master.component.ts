5ef24a4ec86fe6b16e8b24660e41330e4759b24f
rajbattiwala <rajbattiwala@gmail.com>
2021-08-02 18:56:47 +0530

    2Aug

--- src/app/my-components/metal-master/metal-master.component.ts
+++ src/app/my-components/metal-master/metal-master.component.ts
@@ -11,16 +11,52 @@ export class MetalMasterComponent implements OnInit {
   rec: any
   new: any
   recSelect: any
+  recnew: any
   recname: any
   rectype: any
+  rectypenew: any
   recstd: any
   list: any
   selected: boolean = false
   selectedItemsList: any
+  additionalDetails: any
+  additionalDetailsnew: any
+
+  temp:any
+
+  addiData: any
+  addiItem: any
+  bathAddName:any
+  bathAddNameNew:any
+  bathqty:any
+
+  addNewName:any
+  addNewCode:any
 
   constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+
+    this.recnew = [
+      {
+        state: true,
+        name: "",
+        type: "",
+        std: "",
+        elements: [
+          {
+            tag: "P",
+            min: "0.04",
+            max: "0.5"
+          },
+          {
+            tag: "Si",
+            min: "0.15",
+            max: "0.35"
+          }
+        ]
+      }
+    ]
 
     this.rec = [
       {
@@ -229,6 +265,46 @@ export class MetalMasterComponent implements OnInit {
       }
     ]
 
+    this.bathAddName = [
+      {
+        addname: "Ladle Addition",
+        shortcode: "LAD"
+      },
+      {
+        addname: "Nodularization",
+        shortcode: "NOD"
+      },
+      {
+        addname: "Inoculation",
+        shortcode: "INO"
+      }
+    ]
+    
+    this.bathAddNameNew = [
+      {
+        addname: "",
+        shortcode: ""
+      }
+    ]
+
+    this.additionalDetails = [
+      {
+        addname: "",
+        shortcode: "",
+        itemname: "",
+        qty: ""
+      }
+    ]
+
+    this.additionalDetailsnew = [
+      {
+        addname: null,
+        shortcode: null,
+        itemname: null,
+        qty: null
+      }
+    ]
+
     this.fetchSelectedItems()
   }
 
@@ -248,6 +324,40 @@ export class MetalMasterComponent implements OnInit {
 
   openVerticallyCentered(content:any) {
     this.modalService.open(content, { centered: true, scrollable: true, backdropClass: 'dark-blue-backdrop' });
+  }
+
+  saveData(){
+    this.recnew.name = this.recname
+    if(this.rectypenew){
+      this.recnew.type = this.rectypenew  
+    }
+    else{
+      this.recnew.type = this.rectype
+    }
+    this.recnew.std = this.recstd
+    this.rec.push(this.recnew)
+    /*console.log(this.selectedItemsList)
+    console.log(this.rec)*/
+    alert("Saved Successfully!")
+  }
+
+  additionalSave(){
+    this.additionalDetails.addname = this.addiData
+    this.additionalDetails.itemname = this.addiItem
+    this.additionalDetails.qty = this.bathqty
+    
+    this.temp =  this.bathAddName.filter((value:any, index:any) => {
+      return value.addname.match(this.addiData)
+    });
+
+    this.additionalDetails.shortcode = this.temp[0].shortcode
+    this.additionalDetailsnew.push(this.additionalDetails)
+  }
+
+  additionNewNameSave(){
+    this.bathAddNameNew.addname = this.addNewName
+    this.bathAddNameNew.shortcode = this.addNewCode
+    this.bathAddName.push(this.bathAddNameNew)
   }
   
 }
